ccdecdbd25040d25dc646d932394a8fbe0f7a28c
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-17 14:58:32 -0400

    Modules become darker with every successive hover

--- js/app.js
+++ js/app.js
@@ -3,52 +3,55 @@ const container = document.querySelector('.container');
 const browserHeight = window.innerHeight;
 const headerHeight = header.offsetHeight;
 const percentPageHeight = .9;
-const gridColumns = 16;
 const borderWidth = 1;
 let hVal = 0;
-let lVal = 50;
-let hoverCount = 0;
-
 
 setGridDimensions();
 
 function setGridDimensions() {
-    const gridHeight = (browserHeight - headerHeight) * percentPageHeight;
-    container.setAttribute('style', `height: ${gridHeight}px; width: ${gridHeight}px`);
+    const etchASketchHeight = (browserHeight - headerHeight) * percentPageHeight;
+    container.setAttribute('style', `height: ${etchASketchHeight}px; width: ${etchASketchHeight}px`);
     
-    calcModuleDimensions(gridHeight);
+    calcModuleDimensions(etchASketchHeight);
 }
 
-function calcModuleDimensions(gridHeight) {
+function calcModuleDimensions(etchASketchHeight, gridColumns = 16) {
     const totBorderHeight = gridColumns * 2 * borderWidth;
-    const usableGridHeight = gridHeight - totBorderHeight;
-    const moduleHeight = usableGridHeight / gridColumns;
+    const usableEASHeight = etchASketchHeight - totBorderHeight;
+    const moduleHeight = usableEASHeight / gridColumns;
 
-    createModules(moduleHeight);
+    createModules(moduleHeight, gridColumns);
 }
 
-function createModules(moduleHeight) {
+function createModules(moduleHeight, gridColumns) {
     const totModules = gridColumns ** 2;
     
     for (i = 0; i < totModules; i++) {
         const module = document.createElement('div');
         module.setAttribute('style', `height: ${moduleHeight}px; width: ${moduleHeight}px; border-width: ${borderWidth}`);
         module.classList.add('module');
+        module.setAttribute('data-hover-count', '0');
         container.appendChild(module);
     }
 };
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
 
 const modules = document.querySelectorAll('.module');
 modules.forEach(mod => mod.addEventListener('mouseenter', changeColor));
 
 function changeColor(e) {
-    let hslVal = `hsl(${hVal}, 100%, ${lVal}%)`;
-    this.style.backgroundColor = hslVal; 
-    hVal += 3;
-}
+    const elem = e.target;
+    let elemHoverCount = Number(elem.getAttribute('data-hover-count'))
+    let hslVal = '';
 
+    if (elemHoverCount === 0) {
+        hslVal = `hsl(${hVal}, 100%, 50%)`;
+        this.style.backgroundColor = hslVal;
+    } else {
+        lVal = 50 - (elemHoverCount * 5);
+        hslVal = `hsl(${hVal}, 100%, ${lVal}%)`;
+        this.style.backgroundColor = hslVal; 
+    }
+    hVal += 3;
+    
+    elem.setAttribute('data-hover-count', `${elemHoverCount += 1}`);
+}
